7692e8b4276a017e070e09d9a1ca3774e360f80f
BuildTools <unconfigured@null.spigotmc.org>
2019-09-26 22:19:16 +0200

    Added icon

---
+++ app/src/main/java/com/example/assignment1/JobActivity.java
@@ -0,0 +1,16 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text);
+        this.setTitle("Edit Text");
+
+    }
+}
---
+++ app/src/main/java/com/example/assignment1/JobAdapter.java
@@ -0,0 +1,142 @@
+package com.example.assignment1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class JobAdapter extends RecyclerView.Adapter<JobAdapter.ViewHolder> {
+
+    private List<Jobs> mJobs;
+    private Context mContext;
+    private OnJobListener mOnJobListener;
+    private OnJobLongListener mOnJobLongListener;
+
+    public JobAdapter(Context context, List<Jobs> jobs,OnJobListener onJobListener, OnJobLongListener onJobLongListener) {
+        mJobs = jobs;
+        mContext = context;
+        this.mOnJobListener = onJobListener;
+        this.mOnJobLongListener = onJobLongListener;
+    }
+
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public JobAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_demo, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView,mOnJobListener,mOnJobLongListener);
+        return viewHolder;
+    }
+
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        // Get the data model based on position
+        final Jobs jobs = mJobs.get(position);
+
+        // Sætter tækst i hver viewholder
+        viewHolder.txtCompany.setText(jobs.getmCompany());
+        viewHolder.txtTitle.setText(jobs.getmTitle());
+        viewHolder.txtScore.setText(Double.toString(mJobs.get(position).getmScore()));
+
+        // Applied ændrer farve efter om der er søgt eller ej
+
+        if (mJobs.get(position).getmApplied()){
+            viewHolder.txtStatus.setText("APPLIED");
+            viewHolder.txtStatus.setTextColor(Color.GREEN);// should be resource
+        }
+        else{
+            viewHolder.txtStatus.setText("NOT APP");
+            viewHolder.txtStatus.setTextColor(Color.RED);
+        }
+        Log.d("heeeej", mJobs.get(position).getmStatusColor());
+        PorterDuffColorFilter colorfilter = new PorterDuffColorFilter(Color.parseColor(mJobs.get(position).getmStatusColor()), PorterDuff.Mode.MULTIPLY);
+        viewHolder.txtScore.getBackground().setColorFilter(colorfilter);
+
+
+        // Billede i hver viewholder
+        final ImageView imgLogo = viewHolder.imgLogo;
+        final String nameOfImage = "img_"+position;
+        int resId = mContext.getResources().getIdentifier(nameOfImage, "drawable", mContext.getPackageName());
+        Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), resId);
+        imgLogo.setImageBitmap(bitmap);
+
+
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mJobs.size();
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView txtCompany;
+        public TextView txtTitle;
+        public TextView txtStatus;
+        public TextView txtScore;
+        public ConstraintLayout layout;
+        public ImageView imgLogo;
+        OnJobListener onJobListener;
+        OnJobLongListener onJobLongListener;
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView, OnJobListener onJobListener, OnJobLongListener onJobLongListener) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+            itemView.setOnClickListener(this); // this refererer til interfacet
+            itemView.setOnLongClickListener(this);
+            this.onJobListener = onJobListener;
+            this.onJobLongListener = onJobLongListener;
+            txtCompany =    itemView.findViewById(R.id.txtCompany);
+            txtTitle =      itemView.findViewById(R.id.txtJobtitle);
+            txtStatus =     itemView.findViewById(R.id.txtStatus);
+            txtScore =      itemView.findViewById(R.id.txtScore);
+            layout =        itemView.findViewById(R.id.LayoutJobEntry);
+            imgLogo =       itemView.findViewById(R.id.imgLogo);
+        }
+
+        @Override
+        public void onClick (View view){
+            onJobListener.onJobClick(getAdapterPosition());
+        }
+
+        @Override
+        public boolean onLongClick (View view){
+            return onJobLongListener.onJobLongClick(getAdapterPosition());
+        }
+    }
+    public interface OnJobListener{
+        void onJobClick(int position);
+    }
+
+    public interface OnJobLongListener{
+        boolean onJobLongClick(int position);
+    }
+
+}
